9fd2476ba1df36d06e6e9bca66fc0e89c3e50b75
Leonardo Melman <leonardomelman@hotmail.com.ar>
2019-10-24 11:28:34 -0300

    Prueba

--- TPFinal_TOAST/Models/Usuario.cs
+++ TPFinal_TOAST/Models/Usuario.cs
@@ -25,6 +25,7 @@ namespace TPFinal_TOAST.Models
 
         public Usuario()
         {
+
         }
 
         public Usuario(int IDUsuario, string Nombre_Usuario, string Nombre, string Apellido, string Mail, string Contraseña, bool Admin)
