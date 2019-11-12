6f000371412fa4575fb1f06683c7c8e8428e28b3
ULISES ILAN PASERO <44670244@comunidadort.edu.ar>
2019-11-12 07:56:35 -0300

    hehe

--- TPFinal_TOAST/Models/BD.cs
+++ TPFinal_TOAST/Models/BD.cs
@@ -372,30 +372,35 @@ namespace TPFinal_TOAST.Models
             }
             return ListaNumeros;
         }
-        public static Receta TraerRecetaRandom(int num)
+        public static List<Receta> TraerRecetaRandom(List<int> num)
         {
             SqlConnection Conn = Conectar();
             Receta UnaReceta = new Receta();
             List<Ingrediente> Ingredientes = new List<Ingrediente>();
+            List<Receta> recetas = new List<Receta>();
 
             SqlCommand Consulta = Conn.CreateCommand();
             Consulta.CommandType = System.Data.CommandType.StoredProcedure;
             Consulta.CommandText = "TraerRecetaRandom";
-            Consulta.Parameters.Add(new SqlParameter("@NumeroRandom", num));
-            SqlDataReader Lector = Consulta.ExecuteReader();
-            while (Lector.Read())
+            foreach (int n in num)
             {
-                int IDReceta = Convert.ToInt32(Lector["IDReceta"]);
-                string NombreReceta = Lector["NombreReceta"].ToString();
-                int Categoria = Convert.ToInt32(Lector["Categoria"]);
-                string Preparacion = Lector["Preparacion"].ToString();
-                int TiempoPreparacion = Convert.ToInt32(Lector["TiempoPreparacion"]);
-                float CantidadPlatos = Convert.ToInt32(Lector["CantidadPlatos"]);
-                float Dificultad = Convert.ToInt32(Lector["Dificultad"]);
-                string NombreFoto = Lector["Foto"].ToString();
-                HttpPostedFileBase Foto = null;
-                UnaReceta = new Receta(IDReceta, NombreReceta, Categoria, Preparacion, TiempoPreparacion, CantidadPlatos, Dificultad, Foto, NombreFoto, Ingredientes);
-                UnaReceta.Ingredientes = UnaReceta.ListarIngredientes();
+                Consulta.Parameters.Add(new SqlParameter("@NumeroRandom", n));
+                SqlDataReader Lector = Consulta.ExecuteReader();
+                while (Lector.Read())
+                {
+                    int IDReceta = Convert.ToInt32(Lector["IDReceta"]);
+                    string NombreReceta = Lector["NombreReceta"].ToString();
+                    int Categoria = Convert.ToInt32(Lector["Categoria"]);
+                    string Preparacion = Lector["Preparacion"].ToString();
+                    int TiempoPreparacion = Convert.ToInt32(Lector["TiempoPreparacion"]);
+                    float CantidadPlatos = Convert.ToInt32(Lector["CantidadPlatos"]);
+                    float Dificultad = Convert.ToInt32(Lector["Dificultad"]);
+                    string NombreFoto = Lector["Foto"].ToString();
+                    HttpPostedFileBase Foto = null;
+                    UnaReceta = new Receta(IDReceta, NombreReceta, Categoria, Preparacion, TiempoPreparacion, CantidadPlatos, Dificultad, Foto, NombreFoto, Ingredientes);
+                    UnaReceta.Ingredientes = UnaReceta.ListarIngredientes();
+                    recetas.Add(UnaReceta);
+                }
             }
             Desconectar(Conn);
             return UnaReceta;
